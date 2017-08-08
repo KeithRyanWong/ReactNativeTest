c9838453e99dc62fd7a2a491e37afae6fd861627
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-08-08 01:17:13 -0700

    Add client persistence

--- app/store/store.js
+++ app/store/store.js
@@ -1,15 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+import { AsyncStorage } from 'react-native';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import rootReducer from '../reducers/root_reducer';
 
 const defaultState = Object.freeze({
   todos: []
 });
 
-const configureStore = (preloadedState=defaultState) => (
-  createStore(
+const configureStore = (preloadedState=defaultState) => {
+  const store = createStore(
     rootReducer,
-    preloadedState
-  )
-);
+    preloadedState,
+    compose(autoRehydrate())
+  );
+
+  persistStore(store, {storage: AsyncStorage});
+  return store;
+};
 
 export default configureStore;
